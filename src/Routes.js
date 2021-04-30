784ddd728d8d5b5cae8b1df249c46b792a0668a5
Miguel Menezes <miguelmenezes99@hotmail.com>
2021-04-30 10:19:09 -0300

    Termino das Rotas
    - Pagina extra
    - Cadastro
    - Carrinho
    - Comunidades
    - Detalhes (Pagina de um produto so)
    - Home
    - Login
    - Perfil 
    - Produtos

--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,17 @@
 import React from "react";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
+
+//Paginas do site---------------------------------------------------
 import Cadastro from "./Pages/Cadastro";
 import Login from "./Pages/Login";
 import Home from "./Pages/Home";
+import Bio from "./Pages/Bio"
+import Comunidades from "./Pages/Comunidades";
+import Perfil from "./Pages/Perfil";
+import Produtos from "./Pages/Produtos"
+import Carrinho from "./Pages/Carrinho"
+import Detalhes from "./Pages/Detalhes"
+//------------------------------------------------------------------
 
 function Routes(){
     return(
@@ -11,6 +20,12 @@ function Routes(){
                 <Route path = "/login" component={Login}/>
                 <Route path = "/home" component={Home}/>
                 <Route path = "/cadastro" component={Cadastro}/>
+                <Route path = "/bio" component={Bio}/>
+                <Route path = "/comunidades" component={Comunidades}/>
+                <Route path = "/perfil" component={Perfil}/>
+                <Route path = "/Produtos" component={Produtos}/>
+                <Route path = "/Carrinho" component={Carrinho}/>
+                <Route path = "/Detalhes" component={Detalhes}/>
             </Switch>
         </BrowserRouter>
     )
