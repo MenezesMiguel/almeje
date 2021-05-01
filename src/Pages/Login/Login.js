77955c792242c08f7389dcd3723293e40339b128
Miguel Menezes <miguelmenezes99@hotmail.com>
2021-05-01 10:42:51 -0300

    Login Page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,17 +1,31 @@
 import React from "react";
 import "./Login.css"
+import { Form, Button, Nav } from "react-bootstrap";
+
 
 function Login(){
     return(
         <div className="geral">
             <div className="painelInicial">
-                <h1>Atlas deseja boas vindas!</h1>
-                <br></br>
-                <input ></input>
-                <br></br>
-                <input ></input>
-                <br></br>
-                <button>Login</button>
+                <Form>
+                    <img src="./images/Thankyo.png" alt = "Logo"/>
+                    <div className="inputs">
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Atlas deseja Boas Vindas!</Form.Label>
+                            <Form.Control type="email" placeholder="email@email.com" />
+                        </Form.Group>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Control type="password" placeholder="Senha" />
+                        </Form.Group>
+                        <Nav defaultActiveKey="/home" className="flex-column">
+                            <Nav.Link href="/home">Esqueci a senha</Nav.Link>
+                            <Nav.Link eventKey="/cadastro">Não sou cadastrado</Nav.Link>
+                        </Nav>
+                        <Button variant="primary" type="submit">
+                            Login
+                        </Button>
+                    </div>
+                </Form>
             </div>
         </div>
         
