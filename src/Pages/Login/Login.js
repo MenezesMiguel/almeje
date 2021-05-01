b4f7fdbc5add706c582810938fa44d19188b2509
Miguel Menezes <miguelmenezes99@hotmail.com>
2021-05-01 12:24:03 -0300

    Captura de dados de Login
    inserido a funcionalidade de captar os dados que o usuario coloca + rota para a pagina home

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,27 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css"
 import { Form, Button, Nav } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 
 
 function Login(){
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const history = useHistory();
+
+    function Login(){
+        alert("Bem vindo!\n"+ email + "\n" + password);
+        history.push("home");
+    }
     return(
         <div className="geral">
             <div className="painelInicial">
                 <Form>
                     <img src="./images/Thankyo.png" alt = "Logo"/>
                     <div className="inputs">
-                        <Form.Group controlId="formBasicEmail">
-                            <Form.Label>Atlas deseja Boas Vindas!</Form.Label>
-                            <Form.Control type="email" placeholder="email@email.com" />
+                        <Form.Label>Atlas deseja Boas Vindas!</Form.Label>
+                        <Form.Group controlId="email">
+                            <Form.Control type="email" placeholder="Email@email.com" onChange={(e)=>setEmail(e.target.value)}/>
                         </Form.Group>
-
-                            <Form.Control type="password" placeholder="Senha" />
-
+                            <Form.Control type="password" placeholder="Senha" onChange={(e)=>setPassword(e.target.value)}/>
                         <Nav defaultActiveKey="/home" className="flex-column">
                             <Nav.Link href="/home">Esqueci a senha</Nav.Link>
                             <Nav.Link href="/cadastro">Não sou cadastrado</Nav.Link>
                         </Nav>
-                        <Button variant="primary" type="submit" href="/home">
+                        <Button variant="primary" onClick={(Login)}>
                             Login
                         </Button>
                     </div>
