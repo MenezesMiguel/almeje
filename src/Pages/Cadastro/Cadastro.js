5622d88ca93403d5363938281db9131ab881b750
Miguel Menezes <miguelmenezes99@hotmail.com>
2021-05-03 19:51:36 -0300

    Modificacoes nos forms de cadastro

--- src/Pages/Cadastro/Cadastro.js
+++ src/Pages/Cadastro/Cadastro.js
@@ -1,9 +1,68 @@
 import React from "react";
+import "./Cadastro.css";
+import { Form, Button, Col } from "react-bootstrap";
 
-function Cadastro(){
-    return (
-        <h1>Cadastro</h1>
-    );
+function Cadastro() {
+  return (
+    <div className="pagina">
+      <div className="forms">
+        <div className="Teste">
+          <Form>
+            <Form.Row>
+              <Form.Group as={Col} controlId="name">
+                <Form.Label>Nome Completo</Form.Label>
+                <Form.Control placeholder="Nome Completo" />
+              </Form.Group>
+              <Form.Group as={Col} controlId="cpf">
+                <Form.Label>CPF</Form.Label>
+                <Form.Control placeholder="123.456.789-00" />
+              </Form.Group>
+            </Form.Row>
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Email@email.com" />
+              </Form.Group>
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Senha</Form.Label>
+                <Form.Control type="password" placeholder="Senha" />
+              </Form.Group>
+            </Form.Row>
+
+            <Form.Group controlId="formGridAddress1">
+              <Form.Label>Endereco</Form.Label>
+              <Form.Control placeholder="Rua abcd, 123" />
+            </Form.Group>
+
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridCity">
+                <Form.Label>Cidade</Form.Label>
+                <Form.Control />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridState">
+                <Form.Label>Estado</Form.Label>
+                <Form.Control as="select" defaultValue="Selecione">
+                  <option>Selecione</option>
+                  <option>SP</option>
+                  <option>MG</option>
+                </Form.Control>
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridZip">
+                <Form.Label>CEP</Form.Label>
+                <Form.Control />
+              </Form.Group>
+            </Form.Row>
+
+            <Button variant="primary" type="submit">
+              Cadastre-se
+            </Button>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Cadastro;
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,52 +1,47 @@
 import React, { useState } from "react";
-import "./Login.css"
+import "./Login.css";
 import { Form, Button, Nav } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
-import { RiLockPasswordLine } from "react-icons/ri"
 
+function Login() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const history = useHistory();
 
-function Login(){
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
-    const history = useHistory();
-
-
-    function Login(){
-        alert("Bem vindo!\n"+ email + "\n" + password);
-        history.push("home");
-    }
-    return(
-        <div className="geral">
-            <div className="painelInicial">
-                <Form>
-                    <img src="./images/Thankyo.png" alt = "Logo"/>
-                    <div className="inputs">
-                        <Form.Label>Atlas deseja Boas Vindas!</Form.Label>
-                        <Form.Group controlId="email">
-                            <Form.Control 
-                                type="email" 
-                                placeholder="Email@email.com" 
-                                onChange={(e)=>setEmail(e.target.value)}
-                            />
-                        </Form.Group>
-                            <Form.Control 
-                                type="password" 
-                                placeholder = "Senha"
-                                onChange={(e)=>setPassword(e.target.value)}
-                            />
-                         <Nav defaultActiveKey="/home" className="flex-column">
-                            <Nav.Link href="/home">Esqueci a senha</Nav.Link>
-                            <Nav.Link href="/cadastro">Não sou cadastrado</Nav.Link>
-                        </Nav>
-                        <Button variant="primary" onClick={(Login)}>
-                            Login
-                        </Button>
-                    </div>
-                </Form>
-            </div>
-        </div>
-        
-    );
+  function Login() {
+    alert("Bem vindo!\n" + email + "\n" + password);
+    history.push("home");
+  }
+  return (
+    <div className="geral">
+      <div className="painelInicial">
+        <Form>
+          <div className="inputs">
+            <Form.Label>Atlas deseja Boas Vindas!</Form.Label>
+            <Form.Group controlId="email">
+              <Form.Control
+                type="email"
+                placeholder="Email@email.com"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Control
+              type="password"
+              placeholder="Senha"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Nav defaultActiveKey="/home" className="flex-column">
+              <Nav.Link href="/home">Esqueci a senha</Nav.Link>
+              <Nav.Link href="/cadastro">Não sou cadastrado</Nav.Link>
+            </Nav>
+            <Button variant="primary" onClick={Login}>
+              Login
+            </Button>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
