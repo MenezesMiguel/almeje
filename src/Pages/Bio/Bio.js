ed59b7b5a185553c93f42252a5532bc5e9415afd
diegopincer <diegopincer@hotmail.com>
2021-05-13 19:18:24 -0300

    Atualização página Bio #1

--- src/Pages/Bio/Bio.js
+++ src/Pages/Bio/Bio.js
@@ -4,8 +4,10 @@ import "./Bio.css";
 function Bio(){
     return (
         <div className="baseBio">
-            <h1>Quem nós somos</h1>
-            <div className="bioBaixo">
+            <div className="QSTitle">
+                <h1>Quem somos</h1> 
+            </div>
+            <div className="QSBaixo">
                 <div className="foto">
                     <img src={"/images/Quem_somos.jpg"} alt="Quem somos"></img>
                 </div>
@@ -20,33 +22,84 @@ function Bio(){
                     </div>
                 </div>
             </div>
-            <div>
+            <div className="comunidadesTitle">
                 <h1>comunidades</h1>
-                <div className="serra">
-                    <div>
-                    <div>
-                        <h1>Serra</h1>
+            </div>
+            <div className="comunidadesBase">
+                <div className="comunidades">
+                    <div className="comunidadesInfo">
+                        <p className="comunidadesName">Serra - Belho Horizonte/MG</p>
+                        <p className="comunidadesSobre">
+                            O Aglomerado da Serra é a maior favela de Minas Gerais,
+                            localizada em Belo Horizonte, mais precisamente na zona sul da capital.
+                            É classificada como a 3º maior favela do Brasil (atrás apenas da Rocinha,
+                            bairro da Zona Sul do Rio de Janeiro, e da Favela do Sol Nascente em Ceilândia no DF)
+                            e se divide em oito vilas: Nossa Senhora da Conceição . Nossa Senhora de Fátima,
+                            Nossa Senhora Aparecida, Santana do Cafezal, Novo São Lucas, Fazendinha, Chácara,
+                            e Marçola. É a maior favela de Minas com 46 mil habitantes na fonte anexa.
+                            Embora os dados do BNDES deem conta de 46 mil habitantes,
+                            é possível encontrar diferentes apontamentos. O site da Prefeitura de Belo Horizonte (PBH),
+                            por exemplo, contém páginas que relatam a população de 34 mil e outras que citam 50 mil
+                            moradores.O Aglomerado da Serra possui economia diversificada e em evolução contínua.
+                            Uma das áreas mais movimentadas, o cruzamento entre as ruas Bandonion, Nossa Senhora
+                            de Fátima, Serenata e São Sebastião, reúne lojas de roupas femininas, padaria, sorveterias,
+                            supermercado, mercearias, sacolão, drogarias, salões de beleza, mecânica de motos,
+                            consultórios odontológicos, entre outras formas de comércio e prestação de serviço.
+                            O local, conhecido como "Volta", tem movimentação intensa de pessoas, carros, motos, ônibus e caminhões.
+                        </p>
                     </div>
-                    <img src={"/images/Serra.jpeg"} alt="Serra"></img>
+                    <div className="comunidadesImg">
+                        <img src={"/images/Serra.jpeg"} alt="Serra"></img>
                     </div>  
                 </div>
-                <div className="Rocinha">
-                <div>
+                <div className="comunidades">
                     <div>
-                        <h1>Rocinha</h1>
+                        <img src={"/images/Rocinha.jpg"} alt="Rocinha"></img>
                     </div>
-                    <img src={"/images/Rocinha.jpg"} alt="Rocinha"></img>
+                    <div classname="rocinhaTxt">
+                        <p className="comunidadesName">
+                            Rocinha - Rio de Janeiro/RJ
+                        </p>
+                        
+                        <p className="comunidadesSobre">
+                            A Rocinha é uma favela localizada na Zona Sul do município do Rio de Janeiro,
+                            no Brasil. Destaca-se por ser a maior favela do país, contando com cerca de 100
+                            mil habitantes. A região passou a ser considerada um bairro e foi delimitada pela
+                            Lei Nº 1 995 de 18 de junho de 1993, com alterações nos limites dos bairros da Gávea,
+                            Vidigal e São Conrado. O nome advém de uma fazenda, uma "roça" que na década de 1920
+                            foi tomada pela expansão da mancha urbana. Em 1927 foi loteada por Castro Guidas & Cia,
+                            paralisado pela prefeitura, a Rocinha foi crescendo sem nenhuma regularização dos terrenos.
+                            Em 1970, a favela possuía 130 mil habitantes segundo o IBGE. Sendo apelidada como a maior
+                            comunidade sul-americana.
+                        </p>
                     </div>
-                    
                 </div>
-                <div className="Heliópolis">
-                <div>
+                <div className="comunidades">
+                    <div className="comunidadesInfo">
+                        <p className="comunidadesName">
+                            Helipa - São Paulo/SP
+                        </p>
+                        <p className="comunidadesSobre">
+                            Heliópolis é um bairro com uma vila de mesmo nome localizado no distrito
+                            de Sacomã, na Zona Sul da cidade de São Paulo, no Brasil. Composto por catorze
+                            glebas, possui mais de 100.000 habitantes em uma área de quase um milhão de
+                            metros quadrados, é destacada por ser a maior favela da cidade.
+                            Seus limites são a Avenida Juntas Provisórias e se estende até a divisa
+                            com São Caetano do Sul (delimitada pelo rio Tamanduateí) e a Avenida
+                            Guido Aliberti. O transporte público não entra em algumas ruas da favela,
+                            pois as mesmas são estreitas, então as pessoas se deslocam até as vias
+                            principais, como a Estrada das Lágrimas, Avenida Almirante Delamare,
+                            Rua Coronel Silva Castro e Rua Cônego Xavier (Hospital Heliópolis),
+                            onde estão localizados os pontos de ônibus.Heliópolis também fica próxima
+                            à linha verde do metrô (Estação Sacomã) e às principais vias de acesso de
+                            São Paulo, como a Rodovia Anchieta e Avenida do Estado[2]. Dista apenas
+                            8 km do centro de São Paulo. Esses fatores contribuíram para seu rápido
+                            crescimento populacional.
+                        </p>
+                    </div>
                     <div>
-                        <h1>Heliópolis</h1>
+                        <img src={"/images/Heliópolis.jpg"} alt="Heliópolis"></img>
                     </div>
-                    <img src={"/images/Heliópolis.jpg"} alt="Heliópolis"></img>
-                    </div>
-                    
                 </div>
             </div>
         </div>
