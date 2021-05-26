5e906758ecba68ccf78e0e40bad717fa52c961d8
Miguel Menezes <miguelmenezes99@hotmail.com>
2021-05-26 10:59:43 -0300

    Pagina Perfil

--- src/Pages/Perfil/Perfil.js
+++ src/Pages/Perfil/Perfil.js
@@ -1,11 +1,98 @@
-import React from "react"
+import React from "react";
+import { Form, Button, Col } from "react-bootstrap";
 
-function Perfil(){
-    return(
-        <h1>
-            Perfil
-        </h1>
-    );
+import "./Perfil.css";
+
+function Perfil() {
+  return (
+    <div className="geralPerfil">
+      <p className="frase">Meu perfil</p>
+      <div className="Formulario">
+        <p className="AtualizarCadastro">Atualizar Informacoes Pessoais</p>
+        <Form>
+          <Form.Group controlId="name">
+            <Form.Label>Nome Completo</Form.Label>
+            <Form.Control placeholder="Nome completo" />
+          </Form.Group>
+          <Form.Row>
+            <Form.Group as={Col} controlId="nascimento">
+              <Form.Label>Nascimento</Form.Label>
+              <Form.Control placeholder="DD/MM/AAAA" />
+            </Form.Group>
+            <Form.Group as={Col} controlId="cpf">
+              <Form.Label>CPF</Form.Label>
+              <Form.Control placeholder="123.456.789-00" />
+            </Form.Group>
+          </Form.Row>
+          <Button variant="primary" type="submit">
+            Atualizar
+          </Button>
+        </Form>
+        <p className="AtualizarCadastro">Cadastrar endereco</p>
+        <Form>
+          <Form.Group controlId="formGridAddress1">
+            <Form.Label>Endereco</Form.Label>
+            <Form.Control placeholder="Rua abcd, 123" />
+          </Form.Group>
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridCity">
+              <Form.Label>Cidade</Form.Label>
+              <Form.Control
+                placeholder="Cidade"
+              />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridState">
+              <Form.Label>Estado</Form.Label>
+              <Form.Control
+                as="select"
+                defaultValue="Selecione"
+              >
+                <option>Selecione</option>
+                <option>AC</option>
+                <option>AL</option>
+                <option>AP</option>
+                <option>AM</option>
+                <option>BA</option>
+                <option>CE</option>
+                <option>DF</option>
+                <option>ES</option>
+                <option>GO</option>
+                <option>MA</option>
+                <option>MT</option>
+                <option>MS</option>
+                <option>MG</option>
+                <option>PA</option>
+                <option>PB</option>
+                <option>PR</option>
+                <option>PE</option>
+                <option>PI</option>
+                <option>RJ</option>
+                <option>RN</option>
+                <option>RS</option>
+                <option>RO</option>
+                <option>RR</option>
+                <option>SC</option>
+                <option>SP</option>
+                <option>SE</option>
+                <option>TO</option>
+              </Form.Control>
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridZip">
+              <Form.Label>CEP</Form.Label>
+              <Form.Control
+                placeholder="12345-678"
+              />
+            </Form.Group>
+          </Form.Row>
+          <Button variant="primary" type="submit">
+            Atualizar
+          </Button>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
 export default Perfil;
--- src/Routes.js
+++ src/Routes.js
@@ -1,62 +1,63 @@
-import React, { Component } from "react";
-import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 
 //Paginas do site---------------------------------------------------
 import Cadastro from "./Pages/Cadastro";
 import Login from "./Pages/Login";
 import Home from "./Pages/Home";
-import Bio from "./Pages/Bio"
+import Bio from "./Pages/Bio";
 import Perfil from "./Pages/Perfil";
-import Produtos from "./Pages/Produtos"
-import Carrinho from "./Pages/Carrinho"
-import Detalhes from "./Pages/Detalhes"
-import Header from "./Components/Header"
-import Footer from "./Components/Footer"
+import Produtos from "./Pages/Produtos";
+import Carrinho from "./Pages/Carrinho";
+import Detalhes from "./Pages/Detalhes";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 import { isAuthenticated } from "./services/auth";
 //------------------------------------------------------------------
 
-const PrivateRoute = ({component: Component, ...rest}) => {
-    return (
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return (
     <Route
-        {...rest}
-        render={(props) =>
-            isAuthenticated() ? (
-                <Component {...props} />
-            ) : (
-                <Redirect to={{ pathname: "/login", state: { from: props.location } }}/>
-            )
-        }
+      {...rest}
+      render={(props) =>
+        isAuthenticated() ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: "/login", state: { from: props.location } }}
+          />
+        )
+      }
     />
-    )
-}
+  );
+};
 
-function Routes(){
-    return(
-        <BrowserRouter>
-            <Switch>
-                <Route path = "/" component={header}/>
-            </Switch>
-        </BrowserRouter>
-    );
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={header} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 function header() {
-    return (
-        <Header>
-            <Switch>
-                <Route path = "/login" component={Login}/>
-                <Route path = "/home" component={Home}/>
-                <Route path = "/cadastro" component={Cadastro}/>
-                <Route path = "/bio" component={Bio}/>
-                <Route path = "/perfil" component={Perfil}/>
-                <Route path = "/Produtos" component={Produtos}/>
-                <PrivateRoute path = "/carrinho" component={Carrinho}/>
-                <Route path = "/detalhes" component={Detalhes}/>
-                <Route component={()=> <Redirect to="/home"/> } />
-            </Switch>
-            <Footer/>
-        </Header>
-    );
+  return (
+    <Header>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/home" component={Home} />
+        <Route path="/cadastro" component={Cadastro} />
+        <Route path="/bio" component={Bio} />
+        <Route path="/perfil" component={Perfil} />
+        <Route path="/Produtos" component={Produtos} />
+        <PrivateRoute path="/carrinho" component={Carrinho} />
+        <Route path="/detalhes" component={Detalhes} />
+        <Route component={() => <Redirect to="/home" />} />
+      </Switch>
+      <Footer />
+    </Header>
+  );
 }
 
 export default Routes;
