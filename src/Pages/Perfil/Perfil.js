82c39aa2e941bd976bf3966e7803b80cf74d7083
Miguel Menezes <miguelmenezes99@hotmail.com>
2021-05-26 17:50:46 -0300

    Logout

--- src/Pages/Perfil/Perfil.js
+++ src/Pages/Perfil/Perfil.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { ButtonGroup, Button } from "react-bootstrap";
+import { useHistory } from "react-router";
+import {logout} from "../../services/auth";
 
 import "./Perfil.css";
 
 function Perfil() {
+  const history = useHistory();
+  async function Logout() {
+    logout();
+    history.push("/login");
+  }
   return (
     <div className="geralPerfil">
       <p className="frase">Meu perfil</p>
@@ -13,7 +20,7 @@ function Perfil() {
           <a href="/atualizar">
           <Button variant="primary" >Atualizar Cadastro</Button>
           </a>
-          <Button variant="secondary">Logout</Button>
+          <Button variant="secondary" onClick={Logout}>Logout</Button>
           <Button variant="terciary">Deletar Usuario</Button>
         </ButtonGroup>
         <img src="/images/user.png" className="userLogo" />
