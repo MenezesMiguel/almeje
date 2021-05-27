2cc6a8bdc480f09e4693f6e2e4fb65d9c9e2de47
Gustavo Lara dos Santos <gustavosantos@cpejr.com.br>
2021-05-27 15:13:55 -0300

    Modal deletar

--- src/Pages/Perfil/Perfil.js
+++ src/Pages/Perfil/Perfil.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { ButtonGroup, Button } from "react-bootstrap";
+import { ButtonGroup, Button, Modal } from "react-bootstrap";
 import { useHistory } from "react-router";
 import api from "../../services/api";
 import { logout, getUserID } from "../../services/auth";
@@ -8,12 +8,17 @@ import "./Perfil.css";
 
 function Perfil() {
   const history = useHistory();
+  const [data, setData] = useState({});
+  const user_id = getUserID();
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   async function Logout() {
     logout();
     history.push("/login");
   };
-  const [data, setData] = useState({});
-  const user_id = getUserID();
 
   async function getData() {
     const response = await api.get(`/users/${user_id}`);
@@ -24,12 +29,14 @@ function Perfil() {
   useEffect(()=>{
     getData();
   },[]);
+
   async function Deletar() {
+    handleClose();
     const response = await api.delete(`/users/${user_id}`);
     logout();
     alert("Conta deletada com sucesso!!");
     history.push("/login");
-  }
+  };
   return (
     <div className="geralPerfil">
       <p className="frase">Meu perfil</p>
@@ -37,20 +44,40 @@ function Perfil() {
         <div className="ImagemEButtons">
           <ButtonGroup aria-label="Basic example">
             <a href="/atualizar">
-              <Button variant="primary">Atualizar Cadastro</Button>
+              <Button variant="secondary">Atualizar Cadastro</Button>
             </a>
             <Button variant="secondary" onClick={Logout}>
               Logout
             </Button>
-            <Button variant="terciary" onClick={Deletar}>
+            <Button variant="terciary" onClick={handleShow}>
               Deletar Usuario
             </Button>
+            <Modal show={show} onHide={handleClose} centered>
+              <Modal.Header closeButton>
+                <Modal.Title>Tem certeza?</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>Essa ação não poderá ser disfeita.</Modal.Body>
+              <Modal.Footer>
+                <Button variant="terciary" onClick={Deletar}>
+                  Sim
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                  Não
+                </Button>
+              </Modal.Footer>
+            </Modal>
           </ButtonGroup>
-          <img src="/images/user.png" className="userLogo" />
+          <img src="/images/logo.png" className="userLogo" />
         </div>
         <div className="userData">
-          Nome: {data.nome} <p /> Nascimento: {data.nascimento}<p /> CPF: {data.cpf}<p /> Email: {data.email}<p /> Endereço: {data.endereco}
-          <p /> Cidade: {data.cidade}<p /> Estado: {data.estado}<p /> CEP: {data.cep}
+          Nome: <t className="dados">{data.nome}</t><p />
+          Nascimento: <t className="dados">{data.nascimento}</t><p />
+          CPF: <t className="dados">{data.cpf}</t><p />
+          Email: <t className="dados">{data.email}</t><p />
+          Endereço: <t className="dados">{data.endereco}</t><p />
+          Cidade: <t className="dados">{data.cidade}</t><p />
+          Estado: <t className="dados">{data.estado}</t><p />
+          CEP: <t className="dados">{data.cep}</t>
         </div>
       </div>
     </div>
